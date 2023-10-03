8f7ad7b9c236d9521ed844a64070e8c5c49025e2
amel <amelstutic@gmail.com>
2023-10-03 23:56:54 +0200

    add register style

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,7 +1,11 @@
+<link rel="stylesheet" href={{ asset('css//users/register.css') }}>
+
 <x-layout>
+    <div class="mainRegister">
+        <div class="infoRegister">
 
     <h2>Register</h2>
-    <p>Create an account to add or enroll in courses</p>
+    <p>Create an account to add or enroll in courses.</p>
 
     <form method="POST" action="/users?role={{request('role')}}">
     @csrf
@@ -11,9 +15,9 @@
     @error('name')
         <p class="error">{{$message}}</p>
     @enderror
-
+ 
     <br> <br>
-
+ 
     <label for="Email">Email:</label>
     <input type="email" name="email" placeholder="email" value="{{old('email')}}">
     @error('email')
@@ -28,7 +32,7 @@
         <p class="error">{{$message}}</p>
     @enderror
 
-    <br> <br>
+    <br>  <br>
 
     <label for="password2">Confirm password:</label>
     <input type="password" name="password_confirmation" placeholder="confirm password" value="">
@@ -36,28 +40,30 @@
         <p class="error">{{$message}}</p>
     @enderror
 
-        <br> <br>
+        <br>  <br>
 
         @if (!request('role'))
-            <label for="role">Choose desired role:</label> <br>
-            <input type="radio" name="crole" id="student" value="student"> <label for="student">Student</label> <br>
-            <input type="radio" name="crole" id="professor" value="professor"> <label for="professor">Professor</label> <br>
+            <label for="role">Choose desired role:</label> 
+            <input type="radio" name="crole" id="student" value="student"> <label for="student">Student</label>
+            <input type="radio" name="crole" id="professor" value="professor"> <label for="professor">Professor</label> 
             @error('crole')
             <p class="error">The role field is required.</p>
         @enderror
         @endif
 
-        <br> <br>
+        <br>  <br>
 
-    <button type="submit">Sign Up</button>
+    <button class="buttonRegister" type="submit">Sign Up</button>
 
-    <br> <br> <br>
+    <br> <br>
 
     <div>
         <p>Already have an account?</p>
-        <a href="/login">Login</a>
+        <a class="loginRegister" href="/login">Login</a>
     </div>
 
     </form>
 
+</div>
+</div>
 </x-layout>
