9afda08e05e2d6abc755f1484e08c178eb60d746
amel <amelstutic@gmail.com>
2023-10-04 00:56:41 +0200

    add enrollments manage style

--- resources/views/enrollments/enrollments.blade.php
+++ resources/views/enrollments/enrollments.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{asset('css/courses.css')}}">
+<link rel="stylesheet" href="{{asset('css/enrollments/enrollments.css')}}">
 
 @php
     $flag = request('flag');
@@ -6,47 +6,73 @@
 
 <x-layout>
 
-    <x-card>
+    @if($flag)
+    
+    <div class="windowoptionsDelete">
+    <div class="optionsDelete">
+        <h3>Are you sure?</h3>
+    <form method="POST" action="/courses/{{$flag}}">
+     @csrf
+     @method('DELETE')
 
-    <header>
+         <button class="buttonEnrollments">Confirm</button>
+         
+        </form>
+        <a id="cancelButton" href="/enroll/manage?userid={{auth()->user()->id}}"><button class="buttonEnrollments">Cancel</button></a>
+    </div>
+    </div>  
+
+    @else
+
+  <div id="manageMain">
         <h1>My Courses</h1>
-    </header>
+   
 
-    <table>
+    <table class="courses-table">
         <tbody>
         @unless ($enrollments->isEmpty()) 
         @foreach ($enrollments as $enrollment)
             
             <tr>
-                <td>{{$enrollment->course_id}}</td>
+                {{-- <td class="button-cell">{{$enrollment->course_id}}</td> --}}
 
                 @foreach ($courses as $course)
 
                     @if ($course->id == $enrollment->course_id)
-                    <td><a href="/courses/{{$course->id}}">{{$course->title}}</a></td>
+                    <td class="button-cell"><a href="/courses/{{$course->id}}">{{$course->title}}</a></td>
 
-                     <td>
+                     <td class="button-cell">
+                        <div class="full-width-button-container">
                         <a href="/lessons/manage?course={{$course->id}}">
-                            <button>Start lessons</button>
+                            <button class="full-width-button">Start lessons</button>
                         </a>
+                        </div>
                      </td>
 
-                     <td>
+                     <td class="button-cell">
+                        <div class="full-width-button-container">
                         <a href="/questions/test?course={{$course->id}}&userid={{$enrollment->user_id}}">
-                            <button>Test Yourself!</button>
+                            <button class="full-width-button">Test Yourself!</button>
                         </a>
+                        </div>
                      </td>
 
-                     <td>
-                        <a href="/enroll/manage/?userid={{auth()->user()->id}}&flag={{$enrollment->id}}"><button>Abandon</button></a>
-                     </td>
+                     <td class="button-cell">
+                        <div class="full-width-button-container">
+                        <a href="/enroll/manage/?userid={{auth()->user()->id}}&flag={{$enrollment->id}}">
+                            <button class="full-width-button">Abandon</button></a>
+                        </div>
+                    </td>
 
-                     <td>
-                        <a href="/questions/final?course={{$course->id}}&userid={{$enrollment->user_id}}&finished={{$enrollment->finished_lessons}}"><button>Final test</button></a>
-                     </td>
+                     <td class="button-cell">
+                        <div class="full-width-button-container">
+                        <a href="/questions/final?course={{$course->id}}&userid={{$enrollment->user_id}}&finished={{$enrollment->finished_lessons}}">
+                            <button class="full-width-button">Final test</button></a>
+                        </div>
+                    </td>
 
                         @if($enrollment->finished_course == 1)
-                            <td>
+                            <td class="button-cell">
                                 <p>You have finished this course.</p>
                             </td>
                         @endif  
@@ -67,22 +93,17 @@
     </tbody>
     </table>
 
-    </x-card>
+<div class="buttonsEnrollments">
+<div class="infoButtonsEnrollments">
+<a href="/courses"><button class="buttonEnrollments">Find more courses!</button></a>
+<a href="/enroll/history?userid={{auth()->user()->id}}"><button class="buttonEnrollments">My history of courses</button></a>
+</div>
+</div>
 
-<a href="/courses"><button>Find more courses!</button></a>
-<a href="/enroll/history?userid={{auth()->user()->id}}"><button>My history of courses</button></a>
+<a href="/courses"><button class="buttonEnrollments back"><i class="fa-solid fa-arrow-left"></i> Back</button></a>
 
+</div>
 
-    @if($flag)
-    <form method="POST" action="/enroll/{{$flag}}?userid={{auth()->user()->id}}">
-     @csrf
-     @method('DELETE')
-
-         <button>Confirm</button>
-         
-        </form>
-        <a href="/enroll/manage?userid={{auth()->user()->id}}"><button>Cancel</button></a>
-    @endif
-
+@endif
 </x-layout>
 
