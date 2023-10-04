a5657d1e7cbd9f3d64608d8b5915b4eb651f970b
amel <amelstutic@gmail.com>
2023-10-04 23:59:57 +0200

    add users manage style

--- resources/views/users/manage.blade.php
+++ resources/views/users/manage.blade.php
@@ -1,13 +1,43 @@
 <link rel="stylesheet" href={{ asset('css/users/manage.css') }}>
 
+@php
+$flag = request('flag');
+@endphp
+
 <x-layout>
+
+    @if($flag)
+
+    <div class="windowoptionsDelete">
+        <div class="optionsDelete">
+            <h3>Are you sure?</h3>
+        <form method="POST" action="/users/{{$flag}}">
+         @csrf
+         @method('DELETE')
+    
+             <button style="background-color: #192d2e; color:white; padding:0.5em; margin-top:1em">Confirm</button>
+             
+            </form>
+            <a id="cancelButton" href="/courses">
+                <button style="background-color: #192d2e; color:white; padding:0.5em; margin-top:1em">Cancel</button></a>
+        </div>
+        </div>  
+
+    @else
     <div class="usersManage">
   
-        <h2>Manage Users</h2>
+        <div class="headingManage" style="width:100%; display:flex; justify-content:center">
+        <h2 style="border-bottom: 1px solid black; margin-bottom:1em;">Manage Users</h2>
+        </div>
    
 
-    <table>
+    <table class="courses-table">
         <tbody>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
 
             @unless(count($users) == 0)
 
@@ -15,24 +45,28 @@
 
                 <tr>
 
-                    <td>
+                    <td class="button-cell">
+                        <div class="full-width-button-container">
                         <p>{{$user->name}}</p>
+                        </div>
                     </td>
 
-                    <td>
+                    <td class="button-cell">
+                        <div class="full-width-button-container">
                         <p>{{$user->email}}</p>
+                        </div>
                     </td>
 
-                    <td>
+                    <td class="button-cell">
+                        <div class="full-width-button-container">
                         <p>{{$user->role}}</p>
+                        </div>
                     </td>
 
-                    <td>
-                        <form method="POST" action="/users/{{$user->id}}">
-                            @csrf
-                            @method('DELETE')
-                            <button>Delete</button>
-                        </form>
+                    <td class="button-cell">
+                        <a href="/courses/manage/?flag={{$user->id}}">
+                            <button class="full-width-button">Delete</button>
+                        </a>
                     </td>
 
                 </tr>
@@ -52,4 +86,12 @@
     </table>
 
 </div>
+
+<a href="/courses">
+    <button class="backManageUsers" style="background-color: #192d2e; color:white; padding:0.5em; position:absolute; left:10; top:100;">
+        <i class="fa-solid fa-arrow-left"></i> Back</button>
+</a>
+
+@endif
+
 </x-layout>
