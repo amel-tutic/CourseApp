eb10f3dd4705eb29cabeea157005eb2c71a046f0
amel <amelstutic@gmail.com>
2023-10-05 11:30:09 +0200

    font changed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,9 @@
 <html lang="en">
 <head>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300&display=swap" rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/2vdpcukfvfp4kbfrc6pg65pmij26edjxcq840jqf4l3hn52m/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <meta charset="UTF-8">
 
